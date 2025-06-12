f19652dfde58940eda21627d97d0b3dd27b04de6
lah-deh <succssasokere@gmail.com>
2025-06-11 19:42:24 -1100

    first commit

---
+++ React-Task-One/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './app.css';
+import Card from './Card.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <Card 
+        name="Asokere Success"
+        bio={
+          <>
+            I'm an Electronics and Computer Engineering student. <br />
+            Frontend developer in training who loves building and learning new things.
+          </>
+        }
+      />
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
